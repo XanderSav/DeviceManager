8c951ec9dfad087f2c9b99d43dc7f3b3fc244768
XanderSav <alexandersav@wizardsdev.com>
2021-10-22 15:30:26 +0300

    initial. project created

---
+++ internship-swift-2/internship-swift-2/internship_swift_2App.swift
@@ -0,0 +1,17 @@
+//
+//  internship_swift_2App.swift
+//  internship-swift-2
+//
+//  Created by Александр Савченко on 22.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct internship_swift_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
