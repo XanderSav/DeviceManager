e0356375dc636d43f003431bb093041ddcb8cadb
XanderSav <alexandersav@wizardsdev.com>
2021-10-24 14:45:54 +0300

    gitignore, firebase, google-service plist
    added necessary pod files
    added code to configure FIRApp
    added google-servise.plist
    configured .gitignore

--- internship-swift-2/internship-swift-2/internship_swift_2App.swift
+++ internship-swift-2/internship-swift-2/internship_swift_2App.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct internship_swift_2App: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
