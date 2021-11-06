430993f9fa8584707556bc93a75e2644a7b45838
XanderSav <alexandersav@wizardsdev.com>
2021-11-06 21:35:15 +0200

    Added authentication service
    also specified the SignInState enum
    added creation of service instance to app start

---
+++ internship-swift-2/internship-swift-2/Enumerations/SignInState.swift
@@ -0,0 +1,14 @@
+//
+//  SignInState.swift
+//  internship-swift-2
+//
+//  Created by Александр Савченко on 06.11.2021.
+//
+
+import Foundation
+
+public enum SignInState {
+    case initial
+    case signedIn
+    case signedOut
+}
--- internship-swift-2/internship-swift-2/internship_swift_2App.swift
+++ internship-swift-2/internship-swift-2/internship_swift_2App.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 @main
 struct internship_swift_2App: App {
+    
+    @StateObject var authService: AuthenticationService
+    
+    init() {
+        _authService = StateObject(wrappedValue: AuthenticationService(user: User()))
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
