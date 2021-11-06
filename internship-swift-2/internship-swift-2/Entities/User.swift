134ec2ee9f579bd30ad6cc3745513328c5c808e6
XanderSav <alexandersav@wizardsdev.com>
2021-11-06 21:25:48 +0200

    Added entities of device and user

---
+++ internship-swift-2/internship-swift-2/Entities/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  internship-swift-2
+//
+//  Created by Александр Савченко on 06.11.2021.
+//
+
+import Foundation
+
+struct User {
+    var email: String = ""
+    var uid: String = ""
+}
