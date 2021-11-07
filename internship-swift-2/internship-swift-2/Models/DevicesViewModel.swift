2ad5262f7425f96ee0ca35c404a0f47de0b66809
XanderSav <alexandersav@wizardsdev.com>
2021-11-08 00:23:45 +0200

    Update DevicesViewModel.swift

--- internship-swift-2/internship-swift-2/Models/DevicesViewModel.swift
+++ internship-swift-2/internship-swift-2/Models/DevicesViewModel.swift
@@ -6,9 +6,33 @@
 //
 
 import Foundation
+import FirebaseFirestore
 
 
 class DevicesViewModel: ObservableObject {
     
     @Published var devices = [Device]()
+    
+    private var db = Firestore.firestore()
+    
+    func fetchData() {
+        db.collection("devices").addSnapshotListener { (querySnapshot, error) in
+            if (error != nil)
+            {
+                print(error?.localizedDescription)
+            }
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            
+            self.devices = documents.map { (queryDocumentSnapshot) -> Device in
+                let data = queryDocumentSnapshot.data()
+                let UDID = data["UDID"] as? String ?? ""
+                let displayName = data["displayName"] as? String ?? ""
+                let usedBy = data["usedBy"] as? String ?? ""
+                return Device(UDID: UDID, displayName: displayName, usedBy: usedBy)
+            }
+        }
+    }
 }
